6475483788fa6d8c6862fd8747220215e08071ac
Rokas <rokas.marcinkevicius@ktu.edu>
2021-06-17 18:32:43 +0300

    CRUD using CQRS + Mediator Pattern

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,4 +1,6 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Controllers
 {
@@ -6,6 +8,10 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
+        private IMediator _mediator;
 
+        // if _mediator is null then HttpContext is the thing that gets assigned
+        protected IMediator Mediator => _mediator ??=
+            HttpContext.RequestServices.GetService<IMediator>();
     }
 }
