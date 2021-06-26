e43a72e262c9798163a61b6def266810f6d7ccf6
Rokas <rokas.marcinkevicius@ktu.edu>
2021-06-26 22:12:32 +0300

    Finished React  Crud

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -34,7 +34,6 @@ namespace API.Controllers
         public async Task<IActionResult> EditActivity(Guid id, Activity activity)
         {
             activity.Id = id;
-
             return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
         }
 
